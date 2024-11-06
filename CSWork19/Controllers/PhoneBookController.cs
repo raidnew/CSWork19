18a21340406c74c310f0d7fdfbfe5b2ffa1f7f3e
Oleg <oleg@techgame.ru>
2024-11-06 23:39:30 +0300

    Add project files.

---
+++ CSWork19/Controllers/PhoneBookController.cs
@@ -0,0 +1,22 @@
+﻿using CSWork19.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSWork19.Controllers
+{
+    public class PhoneBookController : Controller
+    {
+        private PhoneBookContext _phoneBookContext;
+
+        public IActionResult ContactsList()
+        {
+            _phoneBookContext = new PhoneBookContext();
+            _phoneBookContext.GetItemSourceContacts();
+            return View();
+        }
+
+        public IActionResult ContactsInfo()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSWork19/Models/PhoneBookContext.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Data.Entity;
+
+namespace CSWork19.Models
+{
+    public class PhoneBookContext : DbContext
+    {
+        public PhoneBookContext() : base("DBPhoneBookContext") { }
+
+        public DbSet<Contact> Contacts{ get; set; }
+
+        public BindingList<Contact> GetItemSourceContacts()
+        {
+            Contacts.Load();
+            return Contacts.Local.ToBindingList();
+        }
+    }
+}
